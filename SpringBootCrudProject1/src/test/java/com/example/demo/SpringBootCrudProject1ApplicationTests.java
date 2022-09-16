a3328caac8c1f8fae46224c62e7f793e3ccce2ed
Harish4545 <harishchowdary82@gmail.com>
2022-09-16 13:42:28 +0530

    d

---
+++ SpringBootCrudProject1/src/test/java/com/example/demo/SpringBootCrudProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
